aee1ae04c358717349ce590336d8085ec61d408e
Xintong Cheng <xintongc@gmail.com>
2018-10-31 17:01:10 -0400

    Add files via upload

---
+++ IReportGenerator.java
@@ -0,0 +1,5 @@
+package com.example.com.reporttesting;
+
+public interface IReportGenerator {
+    void generateReport(ReportEntity report);
+}
---
+++ ReportGeneratorService.java
@@ -0,0 +1,22 @@
+package com.example.com.reporttesting;
+
+import java.util.Date;
+
+public class ReportGeneratorService {
+
+    private IReportGenerator reportGenerator;
+    /**
+     * Generate report.
+     * @param startDate start date
+     * @param endDate end date
+     * @param content report content
+     */
+
+    public void generateReport(Date startDate, Date endDate, byte[] content){
+        ReportEntity report = new ReportEntity();
+        report.setContent(content);
+        report.setStartDate(startDate);
+        report.setEndDate(endDate);
+        reportGenerator.generateReport(report);
+    }
+}
